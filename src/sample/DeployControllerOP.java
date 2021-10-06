b735358cabf2eab399dcd157814dbb33c2e41092
Владислав Казанкин <v.kazankin@getqa.ru>
2021-10-06 17:30:27 +0300

    Смена форм вкладки Деплои ОЧ

--- src/sample/DeployControllerOP.java
+++ src/sample/DeployControllerOP.java
@@ -14,6 +14,40 @@ public class DeployControllerOP {
     protected MenuButton standName2op;
     @FXML
     protected MenuButton standName3op;
+
+    @FXML
+    protected MenuItem EIS3op;
+    @FXML
+    protected MenuItem EIS4op;
+    @FXML
+    protected MenuItem EIS5op;
+    @FXML
+    protected MenuItem EIS6op;
+    @FXML
+    protected MenuItem EIS7op;
+
+    @FXML
+    protected MenuItem EIS32op;
+    @FXML
+    protected MenuItem EIS42op;
+    @FXML
+    protected MenuItem EIS52op;
+    @FXML
+    protected MenuItem EIS62op;
+    @FXML
+    protected MenuItem EIS72op;
+
+    @FXML
+    protected MenuItem EIS33op;
+    @FXML
+    protected MenuItem EIS43op;
+    @FXML
+    protected MenuItem EIS53op;
+    @FXML
+    protected MenuItem EIS63op;
+    @FXML
+    protected MenuItem EIS73op;
+
     @FXML
     protected Button addStand2op;
     @FXML
@@ -22,12 +56,18 @@ public class DeployControllerOP {
     protected Button removeStand2op;
     @FXML
     protected Button removeStand3op;
+
     @FXML
-    protected Button openAllDeploys1op;
+    protected Button openAllDeploysEis3Op;
     @FXML
-    protected Button openAllDeploys2op;
+    protected Button openAllDeploysEis4Op;
     @FXML
-    protected Button openAllDeploys3op;
+    protected Button openAllDeploysEis5Op;
+    @FXML
+    protected Button openAllDeploysEis6Op;
+    @FXML
+    protected Button openAllDeploysEis7Op;
+
     @FXML
     protected Button refreshDeploysButton1op;
     @FXML
@@ -48,20 +88,43 @@ public class DeployControllerOP {
     protected DialogPane deployOpModal;
     @FXML
     protected GridPane checkboxTableDeploysOP;
+
     @FXML
-    protected Pane statusDeploysPane1op;
+    protected Tab Eis3OpTab;
     @FXML
-    protected Pane statusDeploysPane2op;
+    protected Tab Eis4OpTab;
     @FXML
-    protected Pane statusDeploysPane3op;
+    protected Tab Eis5OpTab;
+    @FXML
+    protected Tab Eis6OpTab;
+    @FXML
+    protected Tab Eis7OpTab;
+
+    @FXML
+    protected Pane statusDeploysPaneEis3Op;
+    @FXML
+    protected Pane statusDeploysPaneEis4Op;
+    @FXML
+    protected Pane statusDeploysPaneEis5Op;
+    @FXML
+    protected Pane statusDeploysPaneEis6Op;
+    @FXML
+    protected Pane statusDeploysPaneEis7Op;
+
     @FXML
     protected MenuButton deployStatusStandMenuOP;
+
     @FXML
-    protected Label deployStatusStandLabel1op;
+    protected Label deployStatusStandLabelEis3Op;
     @FXML
-    protected Label deployStatusStandLabel2op;
+    protected Label deployStatusStandLabelEis4Op;
     @FXML
-    protected Label deployStatusStandLabel3op;
+    protected Label deployStatusStandLabelEis5Op;
+    @FXML
+    protected Label deployStatusStandLabelEis6Op;
+    @FXML
+    protected Label deployStatusStandLabelEis7Op;
+
     @FXML
     protected Label opProjectName;
     @FXML
@@ -74,81 +137,66 @@ public class DeployControllerOP {
     protected ArrayList<RadioButton> radioGroupListBuildsOP;
 
 
-    public void setDeployStatusStandMenuOP() {
-        deployStatusStandMenuOP.setDisable(false);
-        if (standNameOP.getText().contains("ЕИС")) {
-            deployStatusStandMenuOP.getItems().add(new MenuItem(standNameOP.getText()));
-            deployStatusStandMenuOP.getItems().get(0).setOnAction(event -> setStandNameToDeployStatus1op(standNameOP.getText()));
-        }
-
-        if (standName2op.isVisible() & standName2op.getText().contains("ЕИС")) {
-            deployStatusStandMenuOP.getItems().add(new MenuItem(standName2op.getText()));
-            deployStatusStandMenuOP.getItems().get(1).setOnAction(event -> setStandNameToDeployStatus2op(standName2op.getText()));
-        }
-
-        if (standName3op.isVisible() & standName3op.getText().contains("ЕИС")) {
-            deployStatusStandMenuOP.getItems().add(new MenuItem(standName3op.getText()));
-            deployStatusStandMenuOP.getItems().get(2).setOnAction(event -> setStandNameToDeployStatus3op(standName3op.getText()));
-        }
-    }
 
     protected void clearDeployStatusStandOP() {
         deployStatusStandMenuOP.getItems().clear();
-        deployStatusStandLabel1op.setText("");
-        deployStatusStandLabel2op.setText("");
-        deployStatusStandLabel3op.setText("");
-
+        deployStatusStandLabelEis3Op.setText("");
+        deployStatusStandLabelEis3Op.setText("");
+        deployStatusStandLabelEis3Op.setText("");
     }
 
-    private void setStandNameToDeployStatus1op(final String standName) {
-        deployStatusStandMenuOP.setText(standName);
-        deployStatusStandLabel1op.setText("Стенд " + standName);
-        statusDeploysPane1op.setVisible(true);
-        statusDeploysPane2op.setVisible(false);
-        statusDeploysPane3op.setVisible(false);
-        refreshDeploysButton1op.setDisable(false);
+    /**
+     * Активируем вкладку стенда
+     *
+     * @param standName имя стенда
+     */
+    private void enableStandTab(final String standName) {
+        String shortStand = standName.replace("ЕИС-", "");
+        switch (shortStand) {
+            case("3"):
+                Eis3OpTab.setDisable(false);
+                break;
+            case("4"):
+                Eis4OpTab.setDisable(false);
+                break;
+            case("5"):
+                Eis5OpTab.setDisable(false);
+                break;
+            case("6"):
+                Eis6OpTab.setDisable(false);
+                break;
+            case("7"):
+                Eis7OpTab.setDisable(false);
+                break;
+        }
     }
 
-    private void setStandNameToDeployStatus2op(final String standName) {
-        deployStatusStandMenuOP.setText(standName);
-        deployStatusStandLabel2op.setText("Стенд " + standName);
-        statusDeploysPane1op.setVisible(false);
-        statusDeploysPane2op.setVisible(true);
-        statusDeploysPane3op.setVisible(false);
-        refreshDeploysButton2op.setDisable(false);
-    }
-
-    private void setStandNameToDeployStatus3op(final String standName) {
-        deployStatusStandMenuOP.setText(standName);
-        deployStatusStandLabel3op.setText("Стенд " + standName);
-        statusDeploysPane1op.setVisible(false);
-        statusDeploysPane2op.setVisible(false);
-        statusDeploysPane3op.setVisible(true);
-        refreshDeploysButton3op.setDisable(false);
-    }
-
-    protected void enableDeployStatusPanesOP() {
-        statusDeploysPane1op.setDisable(false);
-        statusDeploysPane2op.setDisable(false);
-        statusDeploysPane3op.setDisable(false);
-    }
-
-    protected void disableDeployStatusPanesOP() {
-        statusDeploysPane1op.setDisable(true);
-        statusDeploysPane2op.setDisable(true);
-        statusDeploysPane3op.setDisable(true);
-    }
-
-    protected void disableRefreshButtonOP() {
-        refreshDeploysButton1op.setDisable(true);
-        refreshDeploysButton2op.setDisable(true);
-        refreshDeploysButton3op.setDisable(true);
-    }
-
-    protected void defaultVisibleDeployStatusPaneOP() {
-        statusDeploysPane1op.setVisible(true);
-        statusDeploysPane2op.setVisible(false);
-        statusDeploysPane3op.setVisible(false);
+    /**
+     * Отключаем вкладку стенда
+     *
+     * @param standName имя стенда
+     */
+    private void disableStandTab(final String standName) {
+        if (standName == null)
+            return;
+        String shortStand = standName.replace("ЕИС-", "");
+        switch (shortStand) {
+            case("3"):
+                Eis3OpTab.setDisable(true);
+                break;
+            case("4"):
+                Eis4OpTab.setDisable(true);
+                break;
+            case("5"):
+                Eis5OpTab.setDisable(true);
+                break;
+            case("6"):
+                Eis6OpTab.setDisable(true);
+                break;
+            case("7"):
+                Eis7OpTab.setDisable(true);
+                break;
+        }
     }
 
     public void disableStandsOP() {
@@ -172,51 +220,81 @@ public class DeployControllerOP {
     }
 
     public void setEIS3op() {
+        disableStandTab(standNameOP.getText());
         standNameOP.setText("ЕИС-3");
+        enableStandTab(standNameOP.getText());
     }
     public void setEIS4op() {
+        disableStandTab(standNameOP.getText());
         standNameOP.setText("ЕИС-4");
+        enableStandTab(standNameOP.getText());
     }
     public void setEIS5op() {
+        disableStandTab(standNameOP.getText());
         standNameOP.setText("ЕИС-5");
+        enableStandTab(standNameOP.getText());
     }
     public void setEIS6op() {
+        disableStandTab(standNameOP.getText());
         standNameOP.setText("ЕИС-6");
+        enableStandTab(standNameOP.getText());
     }
     public void setEIS7op() {
+        disableStandTab(standNameOP.getText());
         standNameOP.setText("ЕИС-7");
+        enableStandTab(standNameOP.getText());
     }
 
     public void setEIS32op() {
+        disableStandTab(standName2op.getText());
         standName2op.setText("ЕИС-3");
+        enableStandTab(standName2op.getText());
     }
     public void setEIS42op() {
+        disableStandTab(standName2op.getText());
         standName2op.setText("ЕИС-4");
+        enableStandTab(standName2op.getText());
     }
     public void setEIS52op() {
+        disableStandTab(standName2op.getText());
         standName2op.setText("ЕИС-5");
+        enableStandTab(standName2op.getText());
     }
     public void setEIS62op() {
+        disableStandTab(standName2op.getText());
         standName2op.setText("ЕИС-6");
+        enableStandTab(standName2op.getText());
     }
     public void setEIS72op() {
+        disableStandTab(standName2op.getText());
         standName2op.setText("ЕИС-7");
+        enableStandTab(standName2op.getText());
     }
 
     public void setEIS33op() {
+        disableStandTab(standName3op.getText());
         standName3op.setText("ЕИС-3");
+        enableStandTab(standName3op.getText());
     }
     public void setEIS43op() {
+        disableStandTab(standName3op.getText());
         standName3op.setText("ЕИС-4");
+        enableStandTab(standName3op.getText());
     }
     public void setEIS53op() {
+        disableStandTab(standName3op.getText());
         standName3op.setText("ЕИС-5");
+        enableStandTab(standName3op.getText());
     }
     public void setEIS63op() {
+        disableStandTab(standName3op.getText());
         standName3op.setText("ЕИС-6");
+        enableStandTab(standName3op.getText());
     }
     public void setEIS73op() {
+        disableStandTab(standName3op.getText());
         standName3op.setText("ЕИС-7");
+        enableStandTab(standName3op.getText());
     }
 
     public void addStand2op() {
@@ -238,6 +316,7 @@ public class DeployControllerOP {
         removeStand2op.setVisible(false);
         addStand2op.setVisible(true);
         addStand3op.setVisible(false);
+        disableStandTab(standName2op.getText());
     }
 
     public void removeStand3op() {
@@ -245,5 +324,6 @@ public class DeployControllerOP {
         addStand3op.setVisible(true);
         removeStand3op.setVisible(false);
         removeStand2op.setVisible(true);
+        disableStandTab(standName3op.getText());
     }
 }
