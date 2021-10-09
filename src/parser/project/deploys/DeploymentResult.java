2f07ab22f71392e2632ce3a753d70414163cdf6f
Владислав Казанкин <v.kazankin@getqa.ru>
2021-10-09 10:38:38 +0300

    Формат для вывода в лог

--- src/parser/project/deploys/DeploymentResult.java
+++ src/parser/project/deploys/DeploymentResult.java
@@ -5,4 +5,14 @@ public class DeploymentResult {
     public String creatorDisplayName;
     public String deploymentState;
     public String lifeCycleState;
+
+    @Override
+    public String toString() {
+        return String.format("{ \n" +
+                "name: " + name + "\n" +
+                "creatorDisplayName: " + creatorDisplayName + "\n" +
+                "deploymentState: " + deploymentState + "\n" +
+                "lifeCycleState: " + lifeCycleState + "\n" +
+                " }");
+    }
 }
