4f3a90aba07f5d72011c41db14594149718ab411
Владислав Казанкин <v.kazankin@getqa.ru>
2022-01-11 22:26:10 +0300

    Убрал лишний код

--- src/updater/DeployerOP.java
+++ src/updater/DeployerOP.java
@@ -56,38 +56,6 @@ public class DeployerOP extends Builder {
     /**
      * Делаем релиз
      */
-    public void createRelease(final int number, final String standNameLocal) {
-        String standNameShort = standNameLocal.replace("ЕИС-", "");
-        for (Project p : buildsOpToDeploy) {
-            for (Environments e : p.environments) {
-                if (e.name.contains(standNameShort)) {
-                    p.setCurrentEnvironment(e);
-                    break;
-                }
-            }
-           /* String json = "{'planResultKey':'EIS-EISRDIKWF40-14'," +
-                    "'name':'release-11.0.0-14'," +
-                    "'nextVersionName':'release-11.0.0-15'}";*/
-            String json = "{\"planResultKey\":\"" + p.buildResult.buildResultKey + "\"," +
-                    "\"name\":\"" + p.branch.shortName + "-" + p.buildResult.buildNumber + "-" + standNameShort + "\"," +
-                    "\"nextVersionName\":\"" + p.branch.shortName + "-" + (p.buildResult.buildNumber + 1) + "-" + standNameShort + "\"}";
-            log.info("JSON: " + json);
-//            String request = "https://ci-sel.dks.lanit.ru/rest/api/latest/deploy/project/65404967/version";
-            String request = "https://ci-sel.dks.lanit.ru/rest/api/latest/deploy/project/" + p.currentEnvironment.deploymentProjectId + "/version";
-            log.info("request: " + request);
-            String response = base.getResponsePost(request, json);
-            log.info("response: " + response);
-            Gson g = new Gson();
-            JsonReader reader = new JsonReader(new StringReader(response));
-            Version version = g.fromJson(reader, Version.class);
-            p.setVersion(version, number);
-            deployOpOnStands.get(number).add(p);
-        }
-    }
-
-    /**
-     * Делаем релиз
-     */
     public void createRelease(Stand stand) {
         Project project = stand.getProjectOp();
         String shortStand = stand.getName().replace("ЕИС-", "");
@@ -113,39 +81,6 @@ public class DeployerOP extends Builder {
         JsonReader reader = new JsonReader(new StringReader(response));
         Version version = g.fromJson(reader, Version.class);
         project.setVersionOp(version);
-    }
-
-    /**
-     * Деплоим билд-план на стенд
-     */
-    public void deploy(final int number) {
-        /*for (String x: projectsEnvironments.keySet()) {
-            for (Environments y: projectsEnvironments.get(x)) {
-                String url = "https://ci-sel.dks.lanit.ru/rest/api/latest/queue/deployment?";
-                if (y.name.toLowerCase(Locale.ROOT).contains(stand.toLowerCase(Locale.ROOT))) {
-                    url += "environmentId=" + y.id;
-                    for (String v: allVersions.keySet())
-                        if (v.equals(x)) {
-                            url += "&versionId=" + String.valueOf(allVersions.get(v));
-                            System.out.println(url);
-                        }
-                }
-            }
-            }*/
-        for (Project p : deployOpOnStands.get(number)) {
-            // String url = "https://ci-sel.dks.lanit.ru/rest/api/latest/queue/deployment?environmentId=&versionId=";
-            String url =
-                    "https://ci-sel.dks.lanit.ru/rest/api/latest/queue/deployment?" +
-                    "environmentId=" + p.currentEnvironment.id +
-                    "&versionId=" + p.version[number].id;
-            log.info("url: " + url);
-            String response = base.getResponsePost(url, "");
-            Gson g = new Gson();
-            JsonReader reader = new JsonReader(new StringReader(response));
-            DeploymentResultId deploymentResultId = g.fromJson(reader, DeploymentResultId.class);
-            p.setDeploymentResultId(deploymentResultId, number);
-            log.info("deploymentResultId: " + deploymentResultId);
-        }
     }
 
     /**
